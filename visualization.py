ea78a34ab63f7f44391b466d37a9499c2c5bf83f
Enguerran-Couderc <enguerrancouderc@gmail.com>
2024-01-24 18:28:36 +0100

    update visualization + add compare generated

--- visualization.py
+++ visualization.py
@@ -1,7 +1,7 @@
 from music21 import stream, meter, key, note, metadata, expressions
 import music21
 
-def visualize(time_signature,keys,melodies):
+def visualize(time_signature,keys,melodies,compare=0):
     score = stream.Score()
     score.metadata = metadata.Metadata(title="Melodies generated")
     N=len(keys)
@@ -11,10 +11,21 @@ def visualize(time_signature,keys,melodies):
     measureDuration=float(time_signature.split('/')[0])
     measureCount=1
     for i in range(N):
-        measure.append(expressions.RehearsalMark(f"Melody {i+1}")) 
-        measure.append(key.Key(keys[i]))
+        if compare!=0 :
+            if i%2==0 :
+                name=f"Melody {compare} generated"
+            else :
+                name=f"Melody {compare} Original"
+        else :
+            name=f"Melody {i+1}"
+        measure.append(expressions.RehearsalMark(name))
+        keysplit=keys[i].split(" ")
+        if len(keysplit)==2:
+            measure.append(key.Key(keysplit[0],keysplit[1]))
+        else:
+            measure.append(key.Key(keys[i]))
         measureTime=0.0
-        for n in melodies[i].split(', '):
+        for n in melodies[i]:
             splitted=n.split('-')
             if len(splitted)==2:
                 note,duration=splitted[0],float(splitted[1])
@@ -45,3 +56,20 @@ def visualize(time_signature,keys,melodies):
     score.append(part)
         
     return score
+
+def compare_generated(i):
+    if i<=0 :
+        print("Pour selectionnez la n-ième musique générée tapez compare_generated(n)")
+    file_path='generated.json'
+    
+    if not(os.path.exists(file_path) and os.path.isfile(file_path)):
+            print(f"The file '{file_path}' does not exist.  Exiting program.")
+            return None
+    with open(file_path,'r') as file: #le fichier crée par extract_data.py
+            generated= json.load(file)
+            
+    time_signature="2/4"                     #à changer !!!
+    melodies=[generated[i-1]["Generated"],generated[i-1]["Original"]]
+    keys=[generated[i-1]["Key"],generated[i-1]["Key"]]
+    
+    return visualize(time_signature,keys,melodies,compare=i)
